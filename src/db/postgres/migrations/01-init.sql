0d189b1211ad6c99f14d2a1b0cb2e2245f06e6ce
yoruwitch <evelyn.fernandes4@gmail.com>
2025-06-29 19:09:21 -0300

    feat: add init migration to create users and transactions table

---
+++ src/db/postgres/migrations/01-init.sql
@@ -0,0 +1,27 @@
+-- Uma migration é um script SQL que é executado para criar ou modificar a estrutura do banco de dados.
+CREATE TABLE IF NOT EXISTS users(
+    ID UUID PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,    
+    last_name VARCHAR(50) NOT NULL,    
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+
+-- Para criar o type:
+DO $$
+
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'transaction_type') THEN
+        CREATE TYPE transaction_type AS ENUM ('EARNING', 'EXPENSE', 'INVESTMENT');
+    END IF;    
+END$$;
+
+
+
+CREATE TABLE IF NOT EXISTS transactions(
+    ID UUID PRIMARY KEY,
+    user_id UUID REFERENCES users(ID) ON DELETE CASCADE, -- relacionamento com a tabela users
+    name VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL, -- pode ter 10 dígitos, 2 após a vírgula
+    type transaction_type NOT NULL
+);
