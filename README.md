3f4f935aaf3e0e86bc68aa14e94457f0eb4ca536
yoruwitch <evelyn.fernandes4@gmail.com>
2025-06-29 17:55:01 -0300

    chore: add pg and dotenv setup

--- README.md
+++ README.md
@@ -48,6 +48,17 @@ npm install
 npm run dev
 ```
 
+### Config the dotev
+Create a `.env` file in the root directory and add your environment variables. Here’s an example:
+
+```plaintext
+DB_USER=<your_database_user>
+DB_PASSWORD=<your_database_password>
+DB_HOST=<your_database_host>
+DB_PORT=<your_database_port>
+DB_NAME=<your_database_name>
+```
+
 The API will be available at `http://localhost:3000`.
 
 ## 📌 Considerations
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const name = "John Doe";
-console.log(name);
+import "dotenv/config.js";
+import e from "express";
+
+import { PostgresHelper } from "./src/db/postgres/helper.js";
+
+const app = e();
+
+app.get("/", async (req, res) => {
+    const results = await PostgresHelper.query("SELECT * FROM users");
+    res.send(JSON.stringify(results));
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
