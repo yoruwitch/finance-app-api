961414bb8103f600a5c76175aa057a6e270f24df
yoruwitch <evelyn.fernandes4@gmail.com>
2025-07-01 20:07:10 -0300

    fix: update server port configuration and improve user creation endpoint

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import "dotenv/config.js";
-import e from "express";
+import express from "express";
 
 import { PostgresHelper } from "./src/db/postgres/helper.js";
 
-const app = e();
+const app = express();
+app.use(express.json());
 
 app.get("/", async (req, res) => {
     const results = await PostgresHelper.query("SELECT * FROM users");
     res.send(JSON.stringify(results));
 });
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+
+app.post("/api/users", async (req, res) => {
+    console.log(req.body);
+    res.status(201).send("User created");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port 8000");
 });
