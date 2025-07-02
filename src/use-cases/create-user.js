30ddf7f8f18e8be8f7e75ff8fb261452f3eb4aee
yoruwitch <evelyn.fernandes4@gmail.com>
2025-07-01 21:37:13 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user.js";
+
+export class CreateuserUseCase {
+    async execute(createUserParams) {
+        // TODO: verificar se o email já está sendo usado
+
+        //gerar ID do usuário
+        const userId = uuidv4();
+
+        // criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+        // inserir o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        };
+
+        // chamar o repositório para salvar o usuário
+        const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+        const createdUser = await postgresCreateUserRepository.createUser(user);
+
+        return createdUser;
+    }
+}
