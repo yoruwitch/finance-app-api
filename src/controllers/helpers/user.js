3d7ddad1a60dbf771f86ecd9a0da7a7bbbb27afa
yoruwitch <evelyn.fernandes4@gmail.com>
2025-07-10 20:09:57 -0300

    feat: add index.js file to controllers and controller helpers

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -20,7 +20,16 @@ export const invalidIdResponse = () => {
     });
 };
 
-export const checkIfPasswordIsValid = () => checkIfPasswordIsValid.length >= 6;
+export const checkIfPasswordIsValid = () => {
+    return (password) =>
+        validator.isStrongPassword(password, {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+        });
+};
 
 export const checkIfEmailIsValid = (email) => validator.isEmail(email);
 
