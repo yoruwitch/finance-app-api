2ef91098cdecb80a196e9ccec934506ec771dba8
yoruwitch <evelyn.fernandes4@gmail.com>
2025-08-16 18:04:05 -0300

    feat: add CreateTransactionController factory

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -1,1 +1,86 @@
-export class CreateTransactionController {}
+import {
+    serverError,
+    badRequest,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    created,
+} from "../helpers";
+
+import validator from "validator";
+export class CreateTransactionController {
+    constructor(createTransactionUseCase) {
+        this.createTransactionUseCase = createTransactionUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body;
+            //  validar a transação
+            const requiredFields = [
+                "id",
+                "user_id",
+                "name",
+                "date",
+                "amount",
+                "type",
+            ];
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({ message: `Missing param: ${field}` });
+                }
+            }
+
+            // as validações de uusário, amount, type
+
+            const userIsValid = checkIfIdIsValid(params.user_id);
+
+            if (!userIsValid) {
+                return invalidIdResponse();
+            }
+
+            if (params.amount <= 0) {
+                return badRequest({
+                    message: "The amount must be greater than 0.",
+                });
+            }
+
+            const amountIsValid = validator.isCurrency(
+                params.amount.toString(),
+                {
+                    digits_after_decimal: [2],
+                    allow_negatives: false,
+                    decimal_separator: ".",
+                },
+            );
+
+            if (!amountIsValid) {
+                return badRequest({
+                    message: "The amount must be a valid currency.",
+                });
+            }
+            const type = params.type.trim().toUpperCase();
+
+            const typeIsValid = ["EARNING", "EXPENSE", "INVESTMENT"].includes(
+                type,
+            );
+
+            if (!typeIsValid) {
+                return badRequest({
+                    message:
+                        "The type must be either 'EARNING', 'EXPENSE' or  'INVESTMENT'.",
+                });
+            }
+
+            const transaction = await this.createTransactionUseCase.execute({
+                ...params,
+                type,
+            });
+
+            return created(transaction);
+        } catch (error) {
+            console.log(error);
+            return serverError(error);
+        }
+    }
+}
