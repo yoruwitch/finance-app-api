eefc2e5d6b50b4d4e4538796ce80a676b487a7dc
yoruwitch <evelyn.fernandes4@gmail.com>
2025-08-20 18:49:25 -0300

    feat: add PostgresGetTransactionsByUserIdRepository

---
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,11 @@
+import { PostgresHelper } from "../../../db/postgres/helper.js";
+
+export class PostgresGetTransactionsByUserId {
+    async execute(userId) {
+        const transactions = await PostgresHelper.query(
+            "SELECT * FROM transactions WHERE user_id = $1",
+            [userId],
+        );
+        return transactions;
+    }
+}
